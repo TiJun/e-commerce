5f0c1b85b4d92bc5ea1f25ffa1290ff89147ecdb
TiJun <9tomek7@gmail.com>
2023-09-25 22:21:34 +0200

    created cart container for all devices

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,26 @@ const API_URL = 'https://dummyjson.com/products/category/smartphones'
 let products
 fetch(API_URL)
 	.then(res => res.json())
-	.then(res => {
-		products = res.products.map(product => {
+	.then(productsRaw => {
+		console.log(productsRaw.products)
+		products = productsRaw.products.map(products => {
 			return {
-				name: product.title,
+				name: products.title,
+				category: products.category,
+				price: products.price,
+				
 			}
 		})
 		console.log(products)
 	})
+const cartButton = document.querySelector('.header__cart-button')
+const cartBackButton = document.querySelector('.cart__button')
+const cart = document.querySelector('.cart')
+const activeCart = () => {
+	cart.classList.add('cart-active')
+}
+const hideCart = () => {
+	cart.classList.remove('cart-active')
+}
+cartButton.addEventListener('click', activeCart)
+cartBackButton.addEventListener('click', hideCart)
